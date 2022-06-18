1230da4044cfff779e00749588114fe5775e292e
WBI <kimotieenja@gmail.com>
2022-06-18 13:43:58 +0900

    some work
    - Rewrote the player script to implement real jump action.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     Rigidbody playerRb;
 
     public Vector3 walkDirection;
+    public Vector3 latestPos;
 
     public bool jumpingNow = false;
     public bool warkingNow = false;
@@ -15,8 +16,11 @@ public class Player : MonoBehaviour
 
     public int jumpPower = 70;
     public int speed = 10;
+    public int gravityPower = -50;
 
     public float x_input, z_input;
+    public float slideTime = 0.3f;
+    public float turnTime = 0.1f;
 
 
     void Start()
@@ -31,21 +35,30 @@ public class Player : MonoBehaviour
         {
             playerRb.AddForce(walkDirection * speed, ForceMode.VelocityChange);
         }
+
+        Vector3 diff = transform.position - latestPos;
+        latestPos = transform.position;
+
+        if(Mathf.Abs(diff.x) > 0.001f || Mathf.Abs(diff.z) > 0.001f)
+        {
+            if (walkDirection == new Vector3(0, 0, 0)) return;
+            Quaternion rot = Quaternion.LookRotation(diff);
+            rot = Quaternion.Slerp(playerRb.transform.rotation, rot, turnTime);
+            this.transform.rotation = rot;
+        }
+
+        Gravity();
     }
 
     void Update()
     {
-        x_input = Input.GetAxis("Horizontal");
-        z_input = Input.GetAxis("Vertical");
+        x_input = Input.GetAxisRaw("Horizontal");
+        z_input = Input.GetAxisRaw("Vertical");
         walkDirection = new Vector3(x_input, 0, z_input).normalized;
+
         //Jump
         if (Input.GetButtonDown("Jump") && !jumpingNow)
             Jump();
-
-        if(walkDirection.magnitude > 0)
-        {
-            transform.rotation = Quaternion.LookRotation(walkDirection);
-        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -60,5 +73,13 @@ public class Player : MonoBehaviour
     {
         playerRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
         jumpingNow = true;
+    }
+
+    void Gravity()
+    {
+        if (jumpingNow)
+        {
+            playerRb.AddForce(new Vector3(0, gravityPower, 0));
+        }
     }
 }
